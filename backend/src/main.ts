a20e54d108fed288a6627b84ed990f2bb36e19ab
tae0y <tae0yp@gmail.com>
2025-02-04 22:41:06 +0900

    update : swagger 페이지 로그인 기능 추가

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as expressBasicAuth from 'express-basic-auth';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //TODO: /swagger 진입 시, 인증을 거치도록 설정. express-basic-auth 패키지 expressBasicAuth 사용 가능.
+
+  // 테스트용 /swagger 미들웨어: 토큰 값이 "aaa"여야 함.
+  app.use(
+    ['/swagger', '/swagger-json'],
+    expressBasicAuth({
+      //TODO: 사용자 계정 정보를 환경변수로 설정하고, 사용자 계정 정보를 환경변수에서 읽어오도록 설정.
+      users: {
+        nimda: 'nimda',
+        sudo: 'sudo',
+      },
+      challenge: true,
+    }),
+  );
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('NestJS API')
@@ -22,7 +36,12 @@ async function bootstrap() {
       },
       'access-token',
     )
-    .addCookieAuth('refresh-token')
+    .addCookieAuth('refresh-token', {
+      type: 'apiKey',
+      name: 'refresh-token',
+      in: 'cookie',
+      description: 'Enter your refresh token',
+    })
     .build();
   const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('swagger', app, swaggerDocument, {
